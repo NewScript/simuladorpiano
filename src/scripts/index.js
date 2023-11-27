06282dd5c6badfaf55bfaae65d86f7410bd1c3c1
Carlos Timoteo Arruda <carlos.t.arruda@gmail.com>
2023-11-27 13:18:29 -0300

    Encerrando o projeto

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,44 @@
 const keys = document.querySelectorAll( '.key ');
+const volumeSlider = document.querySelector( '.volume-slider input' );
+const keysCheck = document.querySelector( '.keys-check input' );
 
+let mapedKeys = [];
 let audio = new Audio();
 
 const playTune = ( key ) => {
     audio.src = `./src/audios/${ key }.wav`;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add('active');
+    setTimeout( () => {
+        clickedKey.classList.remove('active');
+    }, 150 );
+};
+
+const handleVolume = ( e ) => {
+    audio.volume = e.target.value;
+};
+
+const showHideKeys = () => {
+    keys.forEach( ( key ) => {
+        key.classList.toggle( 'hide' );
+    });
 };
 
 keys.forEach( ( key ) => {
     key.addEventListener( 'click', () => {
         playTune( key.dataset.key );
+        mapedKeys.push( key.dataset.key );
     });
 });
 
 document.addEventListener( 'keydown', ( e ) => {
-    playTune( e.key );
+    if( mapedKeys.includes( e.key ) ){
+        playTune( e.key );
+    }
 });
+
+volumeSlider.addEventListener( 'input', handleVolume );
+
+keysCheck.addEventListener( 'click', showHideKeys );
